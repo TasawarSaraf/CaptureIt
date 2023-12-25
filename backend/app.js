66c116e81d85b80882dd19c131e7fdc834a3b680
TasawarSaraf <tazsaraf@gmail.com>
2023-12-25 12:45:38 -0500

    created signup/login route and added frontend

--- backend/app.js
+++ backend/app.js
@@ -8,12 +8,18 @@ const app = express();
 app.use(express.json()); // For parsing application/json
 
 // Configure MySQL connection
-const connection = mysql.createConnection({
+
+dbConfig = {
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME
-});
+}
+
+
+async function getDbConnection() {
+  return await mysql.createConnection(dbConfig);
+}
 
 // Connect to MySQL
 connection.connect(err => {
@@ -26,8 +32,61 @@ connection.connect(err => {
 });
 
 
-// Define routes here
+// Defining connection
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
-  console.log(process.env);
+});
+
+/**-----SignUp and Login Routes ------- */
+app.post('/signup', async(req,res) =>{
+    try{
+      const {username, email, password} = req.body;
+      // hash the password
+      const hashedPassword = await bcrypt.hash(password, 10);
+      const signUpConnection = await getDbConnection();
+      /**We pass in the values */
+      const [insertUserRow] = await signUpConnection.execute(
+      'INSERT INTO Users (Username, Email, PasswordHash) VALUES (?, ?, ?)',
+      [username, email, hashedPassword])
+
+      await signUpConnection.end();
+      res.status(201).send('User created successfully');
+
+    } catch(error){
+        console.error(error);
+        res.status(500).send('Error occurred');
+    }
+});
+
+app.post('/login', async(req,res) =>{
+    try{
+      const {email, password} = req.body;
+      const loginConnection = await getDbConnection();
+
+      /**Always verify if the email works to save time*/
+      /** we will return everything from it and compare the hasedpassword with the one from SQL */
+      const [users] = await loginConnection.execute('SELECT * FROM User WHERE Email = ?',[email]);
+
+      await loginConnection.end();
+
+      if(users.length == 0){
+        return res.status(401).send('User not found.');
+      }
+
+      const user = users[0];
+
+      const isPasswordValid = await bcrypt.compare(password, user.PasswordHash);
+      if(!isPasswordValid){
+        return res.status(401).send('Password Invalid');
+      }
+
+      /**this tokeen will be available for 24h before expiring */
+      const token = jwt.sign({ userId: user.UserID }, process.env.JWT_SECRET_KEY, { expiresIn: '24h' });
+      /**Keychain in iOS */
+      res.send({ token });
+
+
+    } catch(error){
+        res.send(500).send("Error has occurred within signup");
+    }
 });
